470e1dd58298a1634aeaed2ef6abbdbbce3bcb9e
ariel :) <cutiepanda2468@gmail.com>
2026-02-15 12:41:46 -0500

    lesson package seeder small update

--- database/seeders/LessonPackageSeeder.php
+++ database/seeders/LessonPackageSeeder.php
@@ -19,7 +19,7 @@ class LessonPackageSeeder extends Seeder
                 'Description' => 'The ideal 6 week package for complete beginners at tennis. This private training schema features six organized lessons aiming to develop a player from zero to hitting ralleys.',
                 'number_of_lessons' => 6,
                 'price' => 280,
-                'image' => 'https://placehold.co/400x300/4ade80/ffffff?text=Tennis+Advanced',
+                'image' => 'https://placehold.co/400x300/4ade80/ffffff?text=Beginner+Kickstart',
                 'is_active' => true,
             ],
             [
@@ -27,7 +27,7 @@ class LessonPackageSeeder extends Seeder
                 'Description' => 'Get started in competitive tennis with 3 lessons on game play, strategy, and tactics.',
                 'number_of_lessons' => 3,
                 'price' => 150,
-                'image' => 'https://placehold.co/400x300/4ade80/ffffff?text=Tennis+Advanced',
+                'image' => 'https://placehold.co/400x300/4ade80/ffffff?text=Tournament+Prep',
                 'is_active' => true,
             ],
             [
@@ -35,7 +35,7 @@ class LessonPackageSeeder extends Seeder
                 'Description' => 'Commit to a 12 week training schedule. Save 20%!',
                 'number_of_lessons' => 3,
                 'price' => 500,
-                'image' => 'https://placehold.co/400x300/4ade80/ffffff?text=Tennis+Advanced',
+                'image' => 'https://placehold.co/400x300/4ade80/ffffff?text=12+Week+Package',
                 'is_active' => true,
             ]
         ];
